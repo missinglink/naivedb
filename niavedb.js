e4f3d907393e1fd6b491d30554afa83f6f7f5f5c
Peter Johnson <insomnia@rcpt.at>
2015-03-01 19:05:43 +0000

    v1.0.7

--- niavedb.js
+++ niavedb.js
@@ -1,7 +1,8 @@
 
 var fs = require('fs'),
     path = require('path'),
-    through = require('through2');
+    through = require('through2'),
+    databases = {};
 
 module.exports = function( filename, options ){
 
@@ -10,95 +11,95 @@ module.exports = function( filename, options ){
   if( !options.hasOwnProperty('safeMode') ) options.safeMode = true;
   if( !options.hasOwnProperty('pretty') ) options.pretty = true;
 
-  var col = collection();
+  // validate filepath
+  var filepath = path.resolve( filename );
+  var stat = fs.statSync( filepath );
 
-  var niavedb = {
-    synced: false,
-    interval: null,
-    get: function( key ){
-      return col.get( key );
-    },
-    set: function( key, value ){
-      niavedb.synced = false;
-      return col.set( key, value );
-    },
-    del: function( key ){
-      niavedb.synced = false;
-      return col.del( key );
-    },
-    _validatePath: function( done ){
-      var filepath = null;
-      filepath = path.resolve( filename );
-      return filepath;
-    },
-    read: function( done ){
-      try { var filepath = niavedb._validatePath(); }
-      catch ( e ){ return done( e ); }
-      fs.stat( filepath, function( err, stats ){
-        if( err ) return done( e );
+  // allow databases to reused across modules
+  if( !databases.hasOwnProperty(filepath) ){
+
+    // create collection
+    var col = collection();
+
+    var niavedb = {
+      synced: false,
+      interval: null,
+      get: function( key ){
+        return col.get( key );
+      },
+      set: function( key, value ){
+        niavedb.synced = false;
+        return col.set( key, value );
+      },
+      del: function( key ){
+        niavedb.synced = false;
+        return col.del( key );
+      },
+      read: function( done ){
         fs.readFile( filepath, { encoding: 'utf8' }, function( err, data ){
           if( err ) return done( e );
           col = collection( JSON.parse( data ) );
           niavedb.synced = true;
           done( null, col );
         });
-      });
-    },
-    readSync: function(){
-      var filepath = niavedb._validatePath();
-      var stats = fs.statSync( filepath );
-      var data = fs.readFileSync( filepath, { encoding: 'utf8' } );
-      col = collection( JSON.parse( data ) );
-      niavedb.synced = true;
-    },
-    write: function( done ){
-      if( !niavedb.synced ){
-        var str = options.pretty
-          ? JSON.stringify( col.store, null, 2 )
-          : JSON.stringify( col.store );
-        fs.writeFile( path.resolve( filename ), str, function( err ){
-          if( err ) return done( e );
-          niavedb.synced = true;
-          done();
-        });
-      }
-      else return done();
-    },
-    writeSync: function(){
-      if( !niavedb.synced ){
-        var str = options.pretty
-          ? JSON.stringify( col.store, null, 2 )
-          : JSON.stringify( col.store );
-        fs.writeFileSync( path.resolve( filename ), str );
+      },
+      readSync: function(){
+        var data = fs.readFileSync( filepath, { encoding: 'utf8' } );
+        col = collection( JSON.parse( data ) );
         niavedb.synced = true;
+      },
+      write: function( done ){
+        if( !niavedb.synced ){
+          var str = options.pretty
+            ? JSON.stringify( col.store, null, 2 )
+            : JSON.stringify( col.store );
+          fs.writeFile( path.resolve( filename ), str, function( err ){
+            if( err ) return done( e );
+            niavedb.synced = true;
+            done();
+          });
+        }
+        else return done();
+      },
+      writeSync: function(){
+        if( !niavedb.synced ){
+          var str = options.pretty
+            ? JSON.stringify( col.store, null, 2 )
+            : JSON.stringify( col.store );
+          fs.writeFileSync( path.resolve( filename ), str );
+          niavedb.synced = true;
+        }
+      },
+      createWriteStream: function( idprop ){
+        if( 'string' !== typeof idprop ) idprop = 'id';
+        return through.obj( function( chunk, enc, next ){
+          niavedb.set( chunk[idprop], chunk );
+          next();
+        });
+      },
+      close: function(){
+        clearInterval( niavedb.interval );
+        niavedb.writeSync();
       }
-    },
-    createWriteStream: function( idprop ){
-      if( 'string' !== typeof idprop ) idprop = 'id';
-      return through.obj( function( chunk, enc, next ){
-        niavedb.set( chunk[idprop], chunk );
-        next();
-      });
-    },
-    close: function(){
-      clearInterval( niavedb.interval );
-      niavedb.writeSync();
+    };
+
+    // save database
+    databases[filepath] = niavedb;
+
+    // write to disk regularly on interval
+    if( options.writeInterval ){
+      niavedb.interval = setInterval( niavedb.write.bind( null, genericErrorHandler ), options.writeInterval );
     }
-  };
 
-  // write to disk regularly on interval
-  if( options.writeInterval ){
-    niavedb.interval = setInterval( niavedb.write.bind( null, genericErrorHandler ), options.writeInterval );
+    // ensure data is written to disk on process termination
+    if( options.safeMode ){
+      process.on( 'exit', niavedb.close );
+    }
+
+    niavedb.readSync();
   }
 
-  // ensure data is written to disk on process termination
-  if( options.safeMode ){
-    process.on( 'exit', niavedb.close );
-  }
-
-  niavedb.readSync();
-
-  return niavedb;
+  return databases[filepath];
 };
 
 function collection( init ){
@@ -106,13 +107,13 @@ function collection( init ){
   var cache = {
     store: ( 'object' === typeof init ) ? init : {},
     get: function( key ){
-      return cache.store[ key ];
+      return cache.store[key];
     },
     set: function( key, value ){
-      cache.store[ key ] = value;
+      cache.store[key] = value;
     },
     del: function( key ){
-      delete cache.store[ key ];
+      delete cache.store[key];
     }
   };
 
